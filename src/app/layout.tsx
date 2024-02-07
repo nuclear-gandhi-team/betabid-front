d12a920953574cffcf2f76da15cc5e83fdad5027
HJK <89708817+HJyup@users.noreply.github.com>
2024-02-07 19:30:54 +0000

    feature: add betModal (#16)
    Co-authored-by: danyilbutov <danyil.butov@solidgate.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { Toaster } from "@/components/ui/sonner";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Toaster />
     </html>
   );
 }
