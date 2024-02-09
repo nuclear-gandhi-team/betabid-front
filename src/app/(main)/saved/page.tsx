c7e8b7128f48cf3bd3dbebd734efca38ca9d22ee
Buiama <83916242+Buiama@users.noreply.github.com>
2024-02-09 15:34:46 +0100

    feature: added saved page (#27)

--- src/app/(main)/saved/page.tsx
+++ src/app/(main)/saved/page.tsx
@@ -1,8 +1,10 @@
+import { DataTableToolbar } from "@/components/modules/filters/toolbar";
 import PageTitle from "@/components/modules/page-title";
 
 const Page = () => (
-  <div>
+  <div className="flex flex-col gap-y-7 pb-4">
     <PageTitle title="Saved" description="Here you can find your saved lots" />
+    <DataTableToolbar />
   </div>
 );
 
