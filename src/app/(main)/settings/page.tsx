01a697185a793f997996017574af8600aa898438
Buiama <83916242+Buiama@users.noreply.github.com>
2024-02-10 16:05:51 +0100

    fix: settings page (#43)

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -6,6 +6,17 @@ import { toast } from "sonner";
 import { z } from "zod";
 
 import PageTitle from "@/components/modules/page-title";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -171,6 +182,24 @@ const Page = () => {
           <Button type="submit">Save</Button>
         </form>
       </Form>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button>Delete account</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Yes, delete my account</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 };
