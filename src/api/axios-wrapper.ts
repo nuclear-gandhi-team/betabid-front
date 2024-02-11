636a72afcdc8ff802ef8f4b8e4d016ce87b9fa99
Buiama <buialodima@i.ua>
2024-02-11 04:46:10 +0100

    fix: changed baseURL to deploy link

--- src/api/axios-wrapper.ts
+++ src/api/axios-wrapper.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5139/api", //process.env.API_URL,
+  baseURL: process.env.API_URL,
 });
 
 axiosInstance.interceptors.request.use(
